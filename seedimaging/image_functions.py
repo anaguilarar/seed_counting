2805353b5e5426018f7a307e9acbc9d63be03601
anaguilarar <anaguilarar@gmail.com>
2024-05-31 13:10:01 +0900

    apply mask function

--- seedimaging/image_functions.py
+++ seedimaging/image_functions.py
@@ -5,8 +5,16 @@ import random
 import colorsys
 import numpy as np
 import os
-from .general import _apply_mask
 
+def _apply_mask(image, mask, color, alpha=0.5):
+    """Apply the given mask to the image.
+    """
+    for c in range(3):
+        image[:, :, c] = np.where(mask == 1,
+                                  image[:, :, c] *
+                                  (1 - alpha) + alpha * color[c] * 255,
+                                  image[:, :, c])
+    return image
 
 def display_instances_cv(image, boxes, masks, class_ids, class_names,
                       scores=None, title="",
